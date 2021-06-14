625d231a6a0fbbb007f922156f10399f157ff477
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-06-13 19:00:36 -0500

    Add async await code

--- AsyncAwaitTests/AsyncAwaitTests.swift
+++ AsyncAwaitTests/AsyncAwaitTests.swift
@@ -8,26 +8,63 @@
 import XCTest
 @testable import AsyncAwait
 
-class AsyncAwaitTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class RemoteCatFactsLoaderTests: XCTestCase {
+    
+    func test_invoke_fetchesCatFactsClousure() {
+        let sut = RemoteCatFactsLoader()
+        let exp = expectation(description: "Wait for expectation")
+        
+        sut.fetch { catFacts in
+            exp.fulfill()
+            
+            XCTAssertTrue(catFacts!.count == 5)
         }
+        
+        wait(for: [exp], timeout: 5.0)
     }
+    
+    func test_invoke_fetchesCatFactsAsync() async throws {
+        let sut = RemoteCatFactsLoader()
+        
+        let catFacts = try await sut.fetch()
+        
+        XCTAssertTrue(catFacts.count == 5)
+    }
+}
 
+struct CatFactElement: Codable {
+  let text: String
+}
+
+typealias CatFacts = [CatFactElement]
+
+class RemoteCatFactsLoader {    
+    func fetch(completion: @escaping (CatFacts?) -> Void) {
+        let url = URL(string: "https://cat-fact.herokuapp.com/facts")!
+        let request = URLRequest(url: url)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            
+            if let error = error {
+                print("Error en el request: \(error.localizedDescription)")
+            }
+            
+            guard let data = data else { return }
+            let facts = try? JSONDecoder().decode(CatFacts.self, from: data)
+            
+            completion(facts)
+        }
+        
+        dataTask.resume()
+    }
+    
+    func fetch() async throws -> CatFacts {
+        let url = URL(string: "https://cat-fact.herokuapp.com/facts")!
+        let request = URLRequest(url: url)
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let facts = try JSONDecoder().decode(CatFacts.self, from: data)
+        
+        return facts
+    }
 }
